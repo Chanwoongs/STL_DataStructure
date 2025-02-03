8f65180678abe558d3ee6fa4d1c93d1733166745
Chanwoong Bang <tkfkdcksdnd@daum.net>
2025-02-03 15:41:16 +0900

    stl::list

---
+++ STL_DataStructure/3.List/main.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+
+// 아이템 구조체
+struct Item
+{
+    Item(int code = 0, int price = 0)
+        : code(code), price(price)
+    {
+
+    }
+
+    int code = 0;
+    int price = 0;
+};
+
+// 데이터 출력용 연산자 오버로딩
+std::ostream& operator<<(std::ostream& os, const Item& item)
+{
+    return os << "코드: " << item.code << ", 가격: " << item.price;
+}
+
+// 데이터 출력용 연산자 오버로딩
+std::ostream& operator<<(std::ostream& os, const std::list<Item>& items)
+{
+    for (auto& item : items)
+    {
+        os << "코드: " << item.code << ", 가격: " << item.price << '\n';
+    }
+    return os;
+}
+
+struct ItemCodeLessComparer
+{
+    bool operator()(const Item& left, const Item& right)
+    {
+        return left.code < right.code;
+;    }
+};
+
+int main()
+{
+    std::list<Item> itemList;
+
+    // 데이터 추가
+    itemList.emplace_front(Item(1, 200));
+    itemList.emplace_front(Item(2, 3200));
+
+    // 뒤에 추가
+    itemList.emplace_back(Item(3, 10));
+    itemList.emplace_back(Item(4, 1233));
+
+    for (std::list<Item>::iterator iterator = itemList.begin();
+        iterator != itemList.end(); ++iterator)
+    {
+        std::cout << *iterator << '\n';
+    }
+    std::cout << '\n';
+
+    // 삭제
+    itemList.pop_front();
+
+    itemList.emplace_back(Item(5, 5462));
+    itemList.emplace_back(Item(6, 2));
+    itemList.emplace_back(Item(7, 16));
+    std::cout << itemList;
+    std::cout << '\n';
+
+    // 정렬
+    // 1 
+    itemList.sort([](const Item& item1, const Item& item2) -> bool {
+        return item1.price <  item2.price;
+        });
+    std::cout << itemList;
+    std::cout << '\n';
+    // 2
+    itemList.sort(ItemCodeLessComparer());
+    std::cout << itemList;
+    std::cout << '\n';
+}
---
+++ STL_DataStructure/4.ListProblem/main.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <list>
+#include <string>
+
+struct Vector2
+{
+    int x, y;
+
+    Vector2(int x = 0, int y = 0)
+        : x(x), y(y)
+    {
+
+    }
+};
+
+struct Point
+{
+    std::string name;
+    Vector2 position;
+
+    Point(std::string name, Vector2&& position)
+        : name(name)
+    {
+        this->position = std::move(position);
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const std::list<Point>& points)
+{
+    for (auto& point : points)
+    {
+        os << point.name << ": (" << point.position.x << ", " << point.position.y << ")\n";
+    }
+    return os;
+}
+
+std::list<Point>::iterator Find(std::list<Point>& list, const std::string value)
+{
+    for (auto it = list.begin(); it != list.end(); it++)
+    {
+        if ((*it).name == value)
+        {
+            return it;
+        }
+    }
+    return list.end();
+}
+
+int main()
+{
+    std::list<Point> list;
+
+    list.push_back(Point("A", Vector2(50, 200)));
+    list.push_back(Point("B", Vector2(20, 80)));
+    list.push_back(Point("C", Vector2(75, 10)));
+    list.push_back(Point("D", Vector2(130, 80)));
+    list.push_back(Point("E", Vector2(100, 200)));
+
+    std::cout << list;
+
+    if (Find(list, "D") != list.end())
+    {
+        list.insert(Find(list, "D"), Point("F", Vector2(180, 150)));
+    }
+    std::cout << '\n';
+    std::cout << list;
+
+}
+
